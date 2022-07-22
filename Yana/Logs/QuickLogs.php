0e88fe061a13e0e9e47cf784ce17e474240279a8
Salvi Pascual <v-salvadorpasqual@4over.com>
2022-07-22 14:26:31 -0400

    ALWAYS displays any errors and remove mem limit for the error log

--- Yana/Logs/QuickLogs.php
+++ Yana/Logs/QuickLogs.php
@@ -11,6 +11,12 @@ namespace Yana\Logs;
 use Yana\Authentication\QuickAuthentication;
 use Yana\Command\ExecutePhpUnit;
 
+// ALWAYS displays any errors and remove mem limit for the error log
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('memory_limit', -1);
+
 /**
  * Class QuickLogs
  *
